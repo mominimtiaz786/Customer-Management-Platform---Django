86b607c02044f865081a9169068a041bb24e4a47
mominimtiaz786 <21100179@lums.edu.pk>
2021-12-22 15:49:29 +0500

    Working with Urls & Views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<strong>Home Page<strong>")
+
+def product(request):
+    return HttpResponse("Product Page")
+
+def customer(request):
+    return HttpResponse("Customer Page")
