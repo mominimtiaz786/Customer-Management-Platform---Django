a9651986370a26cd39050dd4cdc641a50c413d55
mominimtiaz786 <21100179@lums.edu.pk>
2021-12-23 00:18:16 +0500

    Created Relationships within Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
 
 # Create your models here.
 
@@ -11,6 +12,12 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     CATEGORIES = (
         ('Indoor','Indoor'),
@@ -18,14 +25,18 @@ class Product(models.Model):
     )
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    description = models.TextField( null=True)
+    description = models.TextField( null=True, blank=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORIES)
     date = models.DateField(auto_now_add=True, null=True)
+    tag = models.ManyToManyField(Tag, null=True)
 
     def __str__(self):
         return self.name
 
 class Order(models.Model):
+    customer = models.ForeignKey(Customer, null=True, on_delete=SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=SET_NULL)
+
     STATUSES = (
         ('Pending','Pending'),
         ('Out for Delievery','Out for Delievery'),
@@ -35,4 +46,4 @@ class Order(models.Model):
     date = models.DateField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return str(self.id)
+        return str(self.id) + " ----- "+self.customer.name + " -----> ordered -----> " + self.product.name
