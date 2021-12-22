200975985cc3b37ca013b228219e8386b9d700bd
mominimtiaz786 <21100179@lums.edu.pk>
2021-12-23 01:03:18 +0500

    Dynamically Adding Models Data --- Dashboard & Products pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    
+    total_orders = orders.count()
+    orders_delievered = orders.filter(status="Delieverd").count()
+    orders_pending = orders.filter(status="Pending").count()
+
+    context = {'orders':orders[:5],'customers':customers,'total_orders':total_orders, 'orders_delievered':orders_delievered, 'orders_pending':orders_pending}
+    return render(request,'accounts/dashboard.html', context)
 
 def product(request):
-    return render(request,'accounts/products.html')
+    all_products = Product.objects.all()
+    context = {'products':all_products}
+    return render(request,'accounts/products.html',context)
 
 def customer(request):
     return render(request,'accounts/customer.html')
