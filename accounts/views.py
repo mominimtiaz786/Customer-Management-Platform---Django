88bdce1ade094763712e7d00dbe6a348d61f8eb5
mominimtiaz786 <21100179@lums.edu.pk>
2021-12-23 10:01:48 +0500

    Inline formset for Customer to place order from profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
+from django.forms.models import inlineformset_factory
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import *
 from .forms import OrderForm
+from django.forms import inlineformset_factory
 # Create your views here.
 
 def home(request):
@@ -12,7 +14,7 @@ def home(request):
     orders_delievered = orders.filter(status="Delieverd").count()
     orders_pending = orders.filter(status="Pending").count()
 
-    context = {'orders':orders.order_by('-date'),#[:5],
+    context = {'orders':orders.order_by('-id'),#[:5],
     'customers':customers,'total_orders':total_orders, 'orders_delievered':orders_delievered, 'orders_pending':orders_pending}
     return render(request,'accounts/dashboard.html', context)
 
@@ -27,14 +29,18 @@ def customer(request,id):
     context = {'customer_orders':customer_orders, 'customer':customer, 'order_count':customer_orders.count()}
     return render(request,'accounts/customer.html', context)
 
-def createOrder(request):
-    form = OrderForm
+def createOrder(request, id):
+    customer = Customer.objects.get(id=id)
+    #form = OrderForm(initial={'customer':customer})
+    OrderFormSet = inlineformset_factory(Customer,Order, fields=('product','status'), extra=5)
+    formset = OrderFormSet(queryset = Order.objects.none(),instance=customer)
+
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
-    context = {'form':form}
+    context = {'formset':formset}
     return render(request,'accounts/order_form.html',context)
 
 def updateOrder(request,id):
